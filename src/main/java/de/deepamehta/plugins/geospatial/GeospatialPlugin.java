dbc343a067c7427e07dfbaf3cb0305c4f3e3908e
Jörg Richter <jri@deepamehta.de>
2014-03-13 01:19:53 +0100

    Creates a org.neo4j.gis.spatial.Layer
    No real functionality yet.

--- src/main/java/de/deepamehta/plugins/geospatial/GeospatialPlugin.java
+++ src/main/java/de/deepamehta/plugins/geospatial/GeospatialPlugin.java
@@ -4,6 +4,8 @@ import de.deepamehta.core.osgi.PluginActivator;
 import de.deepamehta.core.service.DeepaMehtaService;
 
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.gis.spatial.EditableLayerImpl;
+import org.neo4j.gis.spatial.Layer;
 import org.neo4j.gis.spatial.SpatialDatabaseService;
 
 import java.util.logging.Logger;
@@ -11,6 +13,10 @@ import java.util.logging.Logger;
 
 
 public class GeospatialPlugin extends PluginActivator {
+
+    // ------------------------------------------------------------------------------------------------------- Constants
+
+    private static final String DEFAULT_LAYER_NAME = "dm4.geospatial.layer";
 
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
@@ -31,6 +37,15 @@ public class GeospatialPlugin extends PluginActivator {
     @Override
     public void init() {
         GraphDatabaseService neo4j = (GraphDatabaseService) dms.getDatabaseVendorObject();
-        SpatialDatabaseService spatialService = new SpatialDatabaseService(neo4j);
+        SpatialDatabaseService spatialDB = new SpatialDatabaseService(neo4j);
+        //
+        Layer layer;
+        if (spatialDB.containsLayer(DEFAULT_LAYER_NAME)) {
+            logger.info("########## Default layer exists already (\"" + DEFAULT_LAYER_NAME + "\")");
+            layer = spatialDB.getLayer(DEFAULT_LAYER_NAME);
+        } else {
+            logger.info("########## Creating default layer (\"" + DEFAULT_LAYER_NAME + "\")");
+            layer = spatialDB.createLayer(DEFAULT_LAYER_NAME, GeoCoordinateEncoder.class, EditableLayerImpl.class);
+        }
     }
 }
